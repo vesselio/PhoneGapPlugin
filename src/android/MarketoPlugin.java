f66d0030ad3f7f5241ab7329e1b839c5ef233a0f
Manish Pande <manish@zubhium.com>
2015-08-12 00:19:14 +0530

    Added documentation

--- src/android/MarketoPlugin.java
+++ src/android/MarketoPlugin.java
@@ -16,12 +16,20 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
+/**
+ * MarketoPlugin is a CordovaPlugin class which
+ * recive all evnets / actions from
+ * @author manish
+ */
 public class MarketoPlugin extends CordovaPlugin {
-  public static final String KEY_ACTION_TYPE = "Action Type";
+
+//Constants for Custom Actions
+public static final String KEY_ACTION_TYPE = "Action Type";
 public static final String KEY_ACTION_DETAILS = "Action Details";
 public static final String KEY_ACTION_METRIC = "Action Metric";
 public static final String KEY_ACTION_LENGTH = "Action Length";
 
+//Constants for Associate Lead
 public static final String KEY_FIRST_NAME = "firstName";
 public static final String KEY_LAST_NAME = "lastName";
 public static final String KEY_ADDRESS = "address";
@@ -31,7 +39,6 @@ public static final String KEY_COUNTRY = "country";
 public static final String KEY_POSTAL_CODE = "postalCode";
 public static final String KEY_GENDER = "gender";
 public static final String KEY_EMAIL = "email";
-
 public static final String KEY_TWITTER = "twitterId";
 public static final String KEY_FACEBOOK = "facebookId";
 public static final String KEY_LINKEDIN = "linkedinId";
@@ -42,6 +49,14 @@ public static final String KEY_FACEBOOK_PROFILE_PIC = "facebookPhotoURL";
 private CallbackContext callbackContext;
 private Activity activityContext;
 
+/*
+* execute is a method which will be
+* called when some action is been called from cordova
+*
+* @param action is a action in String
+* @param args is a jeson object from the calling statement
+* @param _callbackContext is the context of the activity
+*/
 @Override
 public boolean execute(String action, JSONArray args, CallbackContext _callbackContext) throws JSONException {
   try {
@@ -49,6 +64,7 @@ public boolean execute(String action, JSONArray args, CallbackContext _callbackC
     activityContext = this.cordova.getActivity();
     final Marketo marketo = Marketo.getInstance(activityContext);
     Log.d("MarketoSDK", "Action "+action);
+    //if action is initialize then it will initialize the marketo SDK
     if ("initialize".equals(action)) {
       final String MUCHKIN_ID = args.optString(0);
       final String SECRET_KEY = args.optString(1);
@@ -61,6 +77,7 @@ public boolean execute(String action, JSONArray args, CallbackContext _callbackC
         }
       });
       return true;
+      //if action is initializeMarketoPush then it will initialise the application for push notification
     } else if ("initializeMarketoPush".equals(action)) {
       final String PROJECT_ID = args.optString(0);
       this.cordova.getThreadPool().execute(new Runnable() {
@@ -72,6 +89,7 @@ public boolean execute(String action, JSONArray args, CallbackContext _callbackC
           }
       });
       return true;
+      //if action is associateLead then it will create a Lead for this device.
     } else if ("associateLead".equals(action)) {
       final JSONObject jsonObject =  args.getJSONObject(0);
       this.cordova.getThreadPool().execute(new Runnable() {
@@ -80,9 +98,10 @@ public boolean execute(String action, JSONArray args, CallbackContext _callbackC
         public void run() {
           marketo.associateLead(getLead(jsonObject));
           callbackContext.success();
-        }
-    });
-    return true;
+          }
+      });
+      return true;
+      //if action is onStop then it will send the onStop action to MarketoSDK
     } else if ("onStop".equals(action)) {
       this.cordova.getThreadPool().execute(new Runnable() {
 
@@ -93,6 +112,7 @@ public boolean execute(String action, JSONArray args, CallbackContext _callbackC
         }
       });
       return true;
+      //if action is onStart then it will send the onStart action to MarketoSDK
     } else if ("onStart".equals(action)) {
       this.cordova.getThreadPool().execute(new Runnable() {
 
@@ -103,6 +123,7 @@ public boolean execute(String action, JSONArray args, CallbackContext _callbackC
         }
       });
       return true;
+        //if action is reportaction then it will send the respective action to MarketoSDK
     } else if("reportaction".equals(action)){
       final String REPORT_ACTION = args.optString(0);
       final JSONObject json = args.optJSONObject(1);
@@ -115,6 +136,7 @@ public boolean execute(String action, JSONArray args, CallbackContext _callbackC
         }
       });
         return true;
+        //if action is settimeout then it will det the default time out to the value from json
     } else if("settimeout".equals(action)){
       final int TIME_OUT = args.optInt(0);
       this.cordova.getThreadPool().execute(new Runnable() {
@@ -135,6 +157,13 @@ public boolean execute(String action, JSONArray args, CallbackContext _callbackC
   }
 }
 
+/*
+* getMetadata is a method which will create a MarketoActionMetaData object
+* out of the JSONObject passed as a parameter
+*
+* @param JSONObject is a json data of Custom Action
+* @return MarketoActionMetaData object created from the json passes
+*/
 private MarketoActionMetaData getMetadata(JSONObject json) {
   MarketoActionMetaData actionMetaData = new MarketoActionMetaData();
  try {
@@ -162,6 +191,14 @@ private MarketoActionMetaData getMetadata(JSONObject json) {
  }
   return actionMetaData;
 }
+
+/*
+* getLead is a method which will create a MarketoLead object
+* out of the JSONObject passed as a parameter
+*
+* @param JSONObject is a json data related to lead
+* @return MarketoLead object created from the json passes
+*/
 
 private MarketoLead getLead(JSONObject object) {
     MarketoLead lead = new MarketoLead();
